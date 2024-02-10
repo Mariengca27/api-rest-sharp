a90dac289b686ced8ccfc2c19885dbbe1155bc6e
Mariana Machado <marimeltywork@outlook.com>
2024-02-10 19:30:09 -0300

    API Pokemon Method GET Only inside main

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+
+class program
+{
+
+    static void Main(string[] args)
+    {
+
+        InvocarApiPokemon();
+
+
+    }
+
+    private static void InvocarApiPokemon()
+    {
+
+        var pokemonApi = new RestClient($"https://pokeapi.co/api/v2/pokemon/");
+        RestRequest requesteApi = new RestRequest("", Method.Get);
+        var responseApi = pokemonApi.Execute(requesteApi);
+
+
+        if (responseApi.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            Console.WriteLine(responseApi.Content);
+        }
+        else{
+            Console.WriteLine(responseApi.ErrorMessage);
+        }
+        Console.ReadKey();
+
+    }
+
+
+
+}
+
+
